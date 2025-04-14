0110ff36a0cc6e10660d1d38ff2e364bf5d3b990
critj1 <katiacristian520@gmail.com>
2025-04-14 16:01:12 +0200

    d

--- src/test/java/PortafoglioTest.java
+++ src/test/java/PortafoglioTest.java
@@ -4,7 +4,7 @@ import classi.Portafoglio;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class portafoglioTest {
+public class PortafoglioTest {
 
   private Portafoglio portafoglio;
 
