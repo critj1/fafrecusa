1ffbe638066bf5f702b4a543796f158786d41c52
critj1 <katiacristian520@gmail.com>
2025-04-14 15:55:52 +0200

    d

--- src/main/java/classi/Investimenti.java
+++ src/main/java/classi/Investimenti.java
@@ -2,30 +2,29 @@ package classi;
 
 public class Investimenti {
 
-    private int durata;
-    private double soldi;
-    private double guadagno;
+  private int durata;
+  private double soldi;
+  private double guadagno;
 
+  public Investimenti(int durata, double soldi, double guadagno) {
+    this.durata = durata;
+    this.soldi = soldi;
+    this.guadagno = guadagno;
+  }
 
-    public Investimenti(int durata,double soldi,double guadagno){
-        this.durata=durata;
-        this.soldi = soldi;
-        this.guadagno=guadagno;
-    }
+  public void scalaTempo() {
+    this.durata = this.durata - 1;
+  }
 
-    public void scalaTempo (){
-        this.durata = this.durata-1;
-    }
+  public int getTempo() {
+    return this.durata;
+  }
 
-    public int getTempo(){
-        return this.durata;
-    }
+  public double getSoldi() {
+    return this.soldi;
+  }
 
-    public double getSoldi() {
-        return this.soldi;
-    }
-
-    public double getGuadagno(){
-        return this.guadagno;
-    }
+  public double getGuadagno() {
+    return this.guadagno;
+  }
 }
--- src/main/java/classi/Portafoglio.java
+++ src/main/java/classi/Portafoglio.java
@@ -2,25 +2,24 @@ package classi;
 
 public class Portafoglio {
 
-    private double schei;
+  private double schei;
 
-    public Portafoglio(double schei) {
-        this.schei = schei;
-    }
+  public Portafoglio(double schei) {
+    this.schei = schei;
+  }
 
-    public double getSchei() {
-        return this.schei;
-    }
+  public double getSchei() {
+    return this.schei;
+  }
 
+  public void aumentaSchei(double schei) {
+    this.schei += schei;
+  }
 
-    public void aumentaSchei(double schei) {
-        this.schei += schei;
-    }
+  public void decrementaSchei(double schei) {
+    this.schei -= schei;
+  }
 
-    public void decrementaSchei(double schei) {
-        this.schei -= schei;
-    }
-
-    //to string nn serve, uso getSchei e gestistico sul main
+  // to string nn serve, uso getSchei e gestistico sul main
 
 }
